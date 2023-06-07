fe48f100d9b3c29637d8d85623be70904732a61e
André Lisboa <andrelisboapt@hotmail.com>
2023-06-07 18:35:23 +0100

    getters, setters, static methods, abstract classes and private constructors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
-class Department { //creating a new Class
+abstract class Department { //creating a new Class, abstract class to use abstract methods
+    static fiscalYear = 2020
     //private id: string;
     //private name: string;  //property of the class, by default is public
     //private employees: string[] = []; //private means the only access is inside the Class
     protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string){ //constructor is reserved keyword, but works like a function
+    constructor(protected readonly id: string, public name: string){ //constructor is reserved keyword, but works like a function
         //this.name = n; //here we give the value "n" to the property name, so objects created with this class, will have the property 'name' with the value we gave
         //we also have the 'readonly' modifier which means that the value of that property cannot change, for example, the 'id'
     }   
 
-    describe() { //this is a method or constructor functions
-        console.log(`Department (${this.id}): ${this.name}`) //we have to use 'this' to refer to the class property
+    static createEmployee(name: string){ //static method is used to not depend on a instance
+        return {name: name}
     }
+
+    /* describe() { //this is a method or constructor functions
+        console.log(`Department (${this.id}): ${this.name}`) //we have to use 'this' to refer to the class property
+    } */
+
+    abstract describe(this: Department): void; //this method is abstract so it needs to be implemented in every inherit class
 
     addEmployee(employee: string){
         this.employees.push(employee) //will pass a new employee string to our array 'employees'
@@ -30,14 +37,48 @@ class ITDepartment extends Department { //inherit from Department, which means t
         this.admins = admins;
     }
 
+    describe(){
+        console.log('IT Department - ID: ' + this.id)
+    }
 }
 
 class AccountingDepartment extends Department{
-    constructor(id: string, private reports: string[]){
-        super(id, 'Accounting');
-       
+    private lastReport: string;
+    private static instance: AccountingDepartment;
+
+    //we use getters and setters for private atributes, just keep in mind that public atributes are very restrict in adiciona logics
+    get mostRecentReport() { //this is the getter, we use this method to get the value of a private atribute from a class
+        if(this.lastReport){
+            return this.lastReport
+            
+        }
+        throw new Error('No report found.')
     }
 
+    set mostRecentReport(value: string){ //this is the setter, we use this method to give a value to a private atribute form a class
+        if (!value){
+            throw new Error('Please pass in a valid value!')
+        }
+        
+        this.addReport(value)
+    }
+
+    private constructor(id: string, private reports: string[]){
+        super(id, 'Accounting');
+       this.lastReport = reports[0]
+    }
+
+    static getInstance(){ //this way we can only have 1 instance
+        if(AccountingDepartment.instance){
+            return this.instance
+        }
+        this.instance = new AccountingDepartment('d2', []);
+        return this.instance
+    }
+
+    describe(){
+        console.log('Accounting Department - ID: ' + this.id)
+    }
 
     addEmployee(name: string){ //this method overrides the one from the main class
         if (name === 'Max'){
@@ -49,6 +90,7 @@ class AccountingDepartment extends Department{
 
     addReport(text: string){
         this.reports.push(text)
+        this.lastReport = text
     }
 
     printReports(){
@@ -56,6 +98,9 @@ class AccountingDepartment extends Department{
     }
 
 }
+
+const employee1 = Department.createEmployee('Max')
+console.log(employee1, Department.fiscalYear)
 
 const it = new ITDepartment('d1', ['Max']); //creating an object with instance from the class where we pass the value for the 'name'
 //added 'd1' because we added an 'id' property do the Class
@@ -74,9 +119,15 @@ it.addEmployee('Manu');
 it.describe();
 it.printEmployeeInformation();
 
-const accounting = new AccountingDepartment('d2', [])
+//const accounting = new AccountingDepartment('d2', [])
+const accounting = AccountingDepartment.getInstance();
+const accounting1 = AccountingDepartment.getInstance();
 
+console.log(accounting, accounting1) //they are exactly the same since we can't create another instance
+
+accounting.mostRecentReport = "s" //this will trigger the method
 accounting.addReport('Something went wrong...')
+console.log(accounting.mostRecentReport) //we access the getter as a property
 
 accounting.printReports()
 
@@ -86,6 +137,8 @@ console.log(accounting)
 accounting.addEmployee('Max')
 accounting.addEmployee('Manu')
 accounting.printEmployeeInformation()
+
+accounting.describe()
 
 
 
