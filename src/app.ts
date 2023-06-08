cde80bdf575b09254beaf4e4afc4fa413f4ca64b
André Lisboa <andrelisboapt@hotmail.com>
2023-06-08 17:24:58 +0100

    classes with interfaces, extending interfaces, optional properties and parameters

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,53 @@
-interface Person { //interface allow us to define the structure of an object, we can use it as a type check to make sure that the object follows a certain struture
-    name: string; //an interface can't have an initializor, so we don't assign a value
-    age: number;
+//////////////Interfaces as Function Types///////////////////////////////
+//type AddFn = (a: number, b: number) => number; //custom function type
+interface AddFn{
+    (a: number, b: number): number //anonymous function on a interface
+}
 
+let add: AddFn
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+///////////////////////////////////////////////////////////////////////////
+
+
+
+interface Named {
+    readonly name?: string; //an interface can't have an initializor, so we don't assign a value
+    outputName?: string; //properties or methods with "?"" they are marked as optional
+}
+
+
+//interfaces are not reconized by JS only in TS
+interface Greetable extends Named{ //interface allow us to define the structure of an object, we can use it as a type check to make sure that the object follows a certain struture
+    //if we inherit an interface, for example, this Greetable has the name property from Named interface
     greet(phrase: string): void;
 }
 
-let user1: Person; //we use our interface as a type
+class Person implements Greetable{ //this means that this class has a contract, which means, they have to follow certain "rules". We can implement as many interfaces as we want.
+    name?: string; //optional property in a class
+    age = 30;
 
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase: string){
-        console.log(phrase + ' ' + this.name)
+    constructor(n?: string){ //optional parameter
+        if(n){
+            this.name = n;
+
+        }
     }
-};
+
+    greet(phrase: string){
+        if(this.name){
+            console.log(phrase + ' ' + this.name)
+        }else{
+            console.log('Hi!')
+        }
+    }
+}
+
+let user1: Greetable; //we use our interface as a type
+
+user1 = new Person()
+//user1.name = 'Manu' //gives error because is readonly property
 
 user1.greet('Hi there - I am');
